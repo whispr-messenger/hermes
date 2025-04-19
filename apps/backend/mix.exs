3673c9f68d28ca80b85f825be824a6e0fa8ef58b
Dimitri Almon <dimitri.almon@gmail.com>
2025-04-19 17:19:34 +0200

    adding comment post likes friend gesture & more

--- apps/backend/mix.exs
+++ apps/backend/mix.exs
@@ -38,16 +38,17 @@ defmodule WhisperBackend.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:swoosh, "~> 1.5"},
-      {:finch, "~> 0.13", override: true},
-      {:mint, "~> 1.4", override: true},  # Add explicit mint dependency with version constraint
+      # Downgrade finch and mint to versions compatible with OTP 24
+      {:finch, "~> 0.12.0", override: true},
+      {:mint, "~> 1.0.0", override: true},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.0"},  # Use an older version of bandit
-      {:redix, "~> 1.2"},
-      {:castore, "~> 1.0"},
+      {:bandit, "~> 0.9.0"},  # Use an even older version of bandit
+      {:redix, "~> 1.1"},
+      {:castore, "~> 0.1.0"},
       {:bcrypt_elixir, "~> 3.0"}
     ]
   end
